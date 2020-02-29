ad197dd8c45a8fbe51332fd98afba4a5991c6519
Fernando Brandao <fwbrandao@gmail.com>
2020-02-29 10:19:26 +0000

    feat: app bar + textBox

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
 import React from 'react';
-import './App.css';
+import { AppBar, IconButton, Box, Button, Typography, Toolbar, makeStyles }
+  from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
 
-function App() {
+import TextBox from './components/textBox'
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  textBox: {
+    display: 'flex',
+    alignItems: 'center'
+  }
+}));
+
+const App = () => {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Document Analysis NLP
-        </p>
-      </header>
-    </div>
+    <Box className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            Document Analysis NLP
+          </Typography>
+          <Button color="inherit">Upload a file</Button>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+      <Box mt={10} ml={10} mr={10}>
+        <TextBox className={classes.textBox}/>
+      </Box>
+    </Box>
   );
 }
 
