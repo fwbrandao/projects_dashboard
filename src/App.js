ca49c8b18ca91f95375a2948a15a3ba284a5b2d7
Fernando Brandao <fwbrandao@gmail.com>
2020-03-13 21:06:58 +0000

    feat: github home

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import { Box, Typography, makeStyles }
+import { Box, Typography, makeStyles, Tooltip, Link, IconButton }
   from '@material-ui/core';
 import NavBar from './components/navBar/index';
 import BGImage from './images/arif-wahid-y3FkHW1cyBE-unsplash.png';
+import GitHubIcon from '@material-ui/icons/GitHub';
+
 
 const useStyles = makeStyles(theme => ({
   root: {
     height: 'calc(100vh - 64px)',
     flexGrow: 1,
-    backgroundImage:`url(${BGImage})`,
+    backgroundImage: `url(${BGImage})`,
     backgroundSize: 'cover',
     overflow: 'hidden',
-    backgroundRepeat  : 'no-repeat',
+    backgroundRepeat: 'no-repeat',
     backgroundPosition: 'center',
   },
   menuButton: {
@@ -36,6 +38,15 @@ const App = () => {
       <NavBar />
       <Box className={classes.root}>
         <Typography variant="h2" className={classes.title}>Welcome to Data Science</Typography>
+        <Box>
+          <Tooltip title="Navigate to my githug" aria-label="code">
+            <Link href="https://github.com/fwbrandao" target="_blank">
+              <IconButton aria-label="github" >
+                <GitHubIcon color="action" fontSize="large" />
+              </IconButton>
+            </Link>
+          </Tooltip>
+        </Box>
       </Box>
     </Box>
   );
