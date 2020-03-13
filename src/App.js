dc95fbad4625aea69c228f626f500446b6b6ae8d
Fernando Brandao <fwbrandao@gmail.com>
2020-03-13 20:48:30 +0000

    feat: bg image

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import React from 'react';
-import { AppBar, Box, Button, Typography, Toolbar, makeStyles }
+import { Box, Typography, makeStyles }
   from '@material-ui/core';
-import TextBox from './components/textBox'
-import CustomizedMenus from './components/menu/index';
-import AboutProject from './components/aboutProject';
 import NavBar from './components/navBar/index';
+import BGImage from './images/arif-wahid-y3FkHW1cyBE-unsplash.png';
 
 const useStyles = makeStyles(theme => ({
   root: {
+    height: 'calc(100vh - 64px)',
     flexGrow: 1,
+    backgroundImage:`url(${BGImage})`,
+    backgroundSize: 'cover',
+    overflow: 'hidden',
+    backgroundRepeat  : 'no-repeat',
+    backgroundPosition: 'center',
   },
   menuButton: {
     marginRight: theme.spacing(2),
   },
   title: {
+    marginTop: '100px',
     display: "flex",
-    justifyContent: "center"
+    justifyContent: "center",
+    color: 'white'
   },
   textBox: {
     display: 'flex',
@@ -26,9 +32,9 @@ const useStyles = makeStyles(theme => ({
 const App = () => {
   const classes = useStyles();
   return (
-    <Box className={classes.root}>
+    <Box >
       <NavBar />
-      <Box mt={10} ml={10} mr={10}>
+      <Box className={classes.root}>
         <Typography variant="h2" className={classes.title}>Welcome to Data Science</Typography>
       </Box>
     </Box>
