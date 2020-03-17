83c4363fbdcc727582402fec8f0f252682045502
Fernando Brandao <fwbrandao@gmail.com>
2020-03-17 17:22:27 +0000

    feat: project info panel

---
+++ src/components/infoExpansionPanel/index.jsx
@@ -0,0 +1,84 @@
+import React from 'react';
+import {
+  makeStyles,
+  ExpansionPanel,
+  ExpansionPanelDetails,
+  ExpansionPanelSummary,
+  Typography
+} from '@material-ui/core';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: '100%',
+    marginBottom: '15px'
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    flexBasis: '33.33%',
+    flexShrink: 0,
+  },
+  secondaryHeading: {
+    fontSize: theme.typography.pxToRem(15),
+    color: theme.palette.text.secondary,
+  },
+}));
+
+const ControlledExpansionPanels = ({
+  firstHeader,
+  firstText,
+  secondHeader,
+  secondText,
+  thirdHeader,
+  thirdText
+}) => {
+  const classes = useStyles();
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleChange = panel => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
+  };
+
+  return (
+    <div className={classes.root} >
+      <ExpansionPanel expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
+        <ExpansionPanelSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1bh-content"
+          id="panel1bh-header"
+        >
+          <Typography className={classes.heading}>{firstHeader}</Typography>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          <Typography>{firstText}</Typography>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+      <ExpansionPanel expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
+        <ExpansionPanelSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel2bh-content"
+          id="panel2bh-header"
+        >
+          <Typography className={classes.heading}>{secondHeader}</Typography>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          <Typography>{secondText}</Typography>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+      <ExpansionPanel expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>
+        <ExpansionPanelSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel3bh-content"
+          id="panel3bh-header"
+        >
+          <Typography className={classes.heading}>{thirdHeader}</Typography>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          <Typography>{thirdText}</Typography>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+    </div>
+  );
+}
+
+export default ControlledExpansionPanels;
--- src/projects/fakeNewsDetector/main/index.jsx
+++ src/projects/fakeNewsDetector/main/index.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Box, Button } from '@material-ui/core';
 import NavBar from '../../../components/navBar/index';
 import AboutProject from '../../../components/aboutProject/index';
 import TextBox from '../../../components/textBox/index';
+import ControlledExpansionPanels from '../../../components/infoExpansionPanel/index';
 
 const FakeNewsDetector = () => {
+    const [toggled, setToggled] = useState(false);
+
+    const handleToggle = () => setToggled(!toggled)
+
     return (
         <Box>
             <NavBar />
@@ -16,9 +21,40 @@ const FakeNewsDetector = () => {
                 the accuracy score and the confusion matrix tell us how well our model fares."
                 gitHubLink="https://github.com/fwbrandao/Fake_news_detector"
             />
-            <Box mt={5} ml={20} mr={20}>
-                <TextBox />
-                <Button>Submit</Button>
+            <Box mt={2} ml={18} mr={18}>
+                <Button
+                    onClick={handleToggle}
+                    variant="contained" color="primary"
+                >
+                    {!toggled ? 'Project info' : 'Close info'}
+                </Button>
+                {toggled ? <ControlledExpansionPanels
+                    firstHeader='What is a PassiveAggressiveClassifier?'
+                    firstText='Passive Aggressive algorithms are online learning algorithms.
+                Such an algorithm remains passive for a correct classification outcome,
+                and turns aggressive in the event of a miscalculation, updating and adjusting.
+                Unlike most other algorithms, it does not converge.
+                Its purpose is to make updates that correct the loss,
+                causing very little change in the norm of the weight vector.'
+                    secondHeader='What is a TfidfVectorizer?'
+                    secondText='TF (Term Frequency): The number of times a word appears in a document is its Term Frequency.
+                A higher value means a term appears more often than others, and so,
+                the document is a good match when the term is part of the search terms.
+                IDF (Inverse Document Frequency): Words that occur many times a document,
+                but also occur many times in many others, may be irrelevant.
+                IDF is a measure of how significant a term is in the entire corpus.
+                The TfidfVectorizer converts a collection of raw documents into a matrix of TF-IDF features.'
+                    thirdHeader='What is Fake News?'
+                    thirdText='A type of yellow journalism,
+                fake news encapsulates pieces of news that may be hoaxes and is generally spread through social media and other online media.
+                This is often done to further or impose certain ideas and is often achieved with political agendas.
+                Such news items may contain false and/or exaggerated claims, and may end up being viralized by algorithms,
+                and users may end up in a filter bubble.'
+                /> : null}
+                <Box mt={4} >
+                    <TextBox />
+                    <Button>Submit</Button>
+                </Box>
             </Box>
         </Box>
     )
