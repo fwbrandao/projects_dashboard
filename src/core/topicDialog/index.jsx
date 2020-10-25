21985e76028d3843b4d4e5c8a81e4c36730c6829
Fernando Brandao <fwbrandao@gmail.com>
2020-10-25 11:38:15 +0000

    feat: moved project card data to js file

--- src/core/topicDialog/index.jsx
+++ src/core/topicDialog/index.jsx
@@ -17,8 +17,7 @@ import {
 } from '@material-ui/core';
 import CloseIcon from '@material-ui/icons/Close';
 import ProjectCard from '../projectCard';
-import CarDetection from '../../images/self-driving-cars.jpg'
-import ResNets from '../../images/resNets.png'
+import { projectsCardCNN } from './projectCardData';
 
 
 const useStyles = makeStyles(theme => ({
@@ -40,41 +39,6 @@ const useStyles = makeStyles(theme => ({
         marginLeft: theme.spacing(1),
       }
 }));
-
-const projectsCardCNN = [
-    {
-        projectTitle: 'Autonomous driving - Car detection',
-        projectIntro: 'Object detection using the very powerful YOLO model',
-        skills: ['YOLO'],
-        img: CarDetection,
-        link: "/autonomous-driving",
-        gitHub: "https://github.com/fwbrandao/Deep_Learning_Specialization_Coursera/blob/master/Convolutional_Neural_Networks/Week_3/Autonomous_driving_application_Car_detection_v3a.ipynb",
-    },
-    {
-        projectTitle: 'Residual Networks (ResNets)',
-        projectIntro: 'Use ResNets to build very deep CNNs',
-        skills: ['Keras', 'Python', 'TensorFlow'],
-        img: ResNets,
-        link: "/resnets",
-        gitHub: "https://github.com/fwbrandao/Deep_Learning_Specialization_Coursera/blob/master/Convolutional_Neural_Networks/Week_2/Residual_Networks_v2a.ipynb"
-    },
-    {
-        projectTitle: 'ConvNet & TensorFlow application',
-        projectIntro: 'ConvNet in TensorFlow for a classification problem',
-        skills: ['Python', 'TensorFlow'],
-        img: CarDetection,
-        link: "/convnet-and-tensorflow",
-        gitHub: "https://github.com/fwbrandao/Deep_Learning_Specialization_Coursera/blob/master/Convolutional_Neural_Networks/Week_1/Convolution_model_Application.ipynb",
-    },
-    {
-        projectTitle: 'Keras introduction',
-        projectIntro: 'Emotion Detection in Images of Faces',
-        skills: ['Keras', 'Python', 'TensorFlow'],
-        img: CarDetection,
-        link: "/keras-intro",
-        gitHub: "https://github.com/fwbrandao/Deep_Learning_Specialization_Coursera/blob/master/Convolutional_Neural_Networks/Week_2/Keras_Tutorial_v2a.ipynb"
-    },
-  ]
 
 const Transition = React.forwardRef(function Transition(props, ref) {
     return <Slide direction="up" ref={ref} {...props} />;
---
+++ src/core/topicDialog/projectCardData.js
@@ -0,0 +1,37 @@
+import CarDetection from '../../images/self-driving-cars.jpg'
+import ResNets from '../../images/resNets.png'
+
+export const projectsCardCNN = [
+    {
+        projectTitle: 'Autonomous driving - Car detection',
+        projectIntro: 'Object detection using the very powerful YOLO model',
+        skills: ['YOLO'],
+        img: CarDetection,
+        link: "/autonomous-driving",
+        gitHub: "https://github.com/fwbrandao/Deep_Learning_Specialization_Coursera/blob/master/Convolutional_Neural_Networks/Week_3/Autonomous_driving_application_Car_detection_v3a.ipynb",
+    },
+    {
+        projectTitle: 'Residual Networks (ResNets)',
+        projectIntro: 'Use ResNets to build very deep CNNs',
+        skills: ['Keras', 'Python', 'TensorFlow'],
+        img: ResNets,
+        link: "/resnets",
+        gitHub: "https://github.com/fwbrandao/Deep_Learning_Specialization_Coursera/blob/master/Convolutional_Neural_Networks/Week_2/Residual_Networks_v2a.ipynb"
+    },
+    {
+        projectTitle: 'ConvNet & TensorFlow application',
+        projectIntro: 'ConvNet in TensorFlow for a classification problem',
+        skills: ['Python', 'TensorFlow'],
+        img: CarDetection,
+        link: "/convnet-and-tensorflow",
+        gitHub: "https://github.com/fwbrandao/Deep_Learning_Specialization_Coursera/blob/master/Convolutional_Neural_Networks/Week_1/Convolution_model_Application.ipynb",
+    },
+    {
+        projectTitle: 'Keras introduction',
+        projectIntro: 'Emotion Detection in Images of Faces',
+        skills: ['Keras', 'Python', 'TensorFlow'],
+        img: CarDetection,
+        link: "/keras-intro",
+        gitHub: "https://github.com/fwbrandao/Deep_Learning_Specialization_Coursera/blob/master/Convolutional_Neural_Networks/Week_2/Keras_Tutorial_v2a.ipynb"
+    },
+  ]
