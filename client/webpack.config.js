2f6859dc544fba1accd2926b1f06d2af0101d03e
fwbrandao <fwbrandaobeta@gmail.com>
2021-09-10 11:36:13 +0100

    feat: added output field in the webpack.config.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,3 +1,12 @@
+const path = require('path')
+
 module.exports = {
-    entry: './index.js'
+    entry: './index.js',
+    module: {
+        rules: []
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index_bundle.js'
+    }
 }
