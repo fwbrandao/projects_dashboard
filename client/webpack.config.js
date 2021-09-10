665ddeb9a0e70b2e95bdd8b16edaa3e391c87d6d
fwbrandao <fwbrandaobeta@gmail.com>
2021-09-10 11:45:26 +0100

    feat: added mode: process.env.NODE_ENV

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
 
 module.exports = {
     entry: './index.js',
@@ -8,5 +10,12 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'index_bundle.js'
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin(),
+        new webpack.EnvironmentPlugin({
+            'NODE_ENV': 'production'
+        })
+    ],
+    mode: process.env.NODE_ENV === 'production' ? 'production' : 'development'
 }
