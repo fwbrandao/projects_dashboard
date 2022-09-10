65dcae4d3bfa224a7fe7f3bb3a35a0b28224409a
fwbrandao <fwbrandaobeta@gmail.com>
2022-09-10 18:55:20 +0100

    fix: removed box text

--- src/components/projects/abstractiveSummarisation/main/index.jsx
+++ src/components/projects/abstractiveSummarisation/main/index.jsx
@@ -45,8 +45,7 @@ const AbstractiveSummarisation = () => {
             rowsMax={4}
             aria-label="maximum height"
             placeholder="Maximum 4 rows"
-            defaultValue="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-                ut labore et dolore magna aliqua."
+            defaultValue=""
         />
     </Box>
     )}
