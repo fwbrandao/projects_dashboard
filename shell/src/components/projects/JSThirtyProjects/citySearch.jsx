ef0e4aaa0b66613e6b7658c666f0f7422a7ba8f8
fwbrandao <fwbrandaobeta@gmail.com>
2022-10-10 22:29:15 +0100

    working search

--- shell/src/components/projects/JSThirtyProjects/citySearch.jsx
+++ shell/src/components/projects/JSThirtyProjects/citySearch.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   Box,
   makeStyles,
 } from '@material-ui/core';
 import City from "../../../images/city.jpg";
+import { useCallback } from "react";
 
 const useStyles = makeStyles(() => ({
   root: {
@@ -25,7 +26,7 @@ const useStyles = makeStyles(() => ({
     outline: 0,
     border: "10px solid #F7F7F7",
     width: "120%",
-    left: "-10%",
+    left: "-17%",
     position: "relative",
     top: "10px",
     zIndex: 2,
@@ -41,26 +42,97 @@ const useStyles = makeStyles(() => ({
     margin: 0,
     padding: 0,
     position: "relative",
+    "& li": {
+      background: "white",
+      listStyle: "none",
+      borderBottom: "1px solid #D8D8D8",
+      boxShadow: "0 0 10px rgba(0, 0, 0, 0.14)",
+      margin: 0,
+      padding: "20px",
+      transition: "background 0.2s",
+      display: "flex",
+      justifyContent: "space-between",
+      textTransform: "capitalize",
+    },
+    "& li:nth-child(even)": {
+      transform: "perspective(100px) rotateX(3deg) translateY(2px) scale(1.001)",
+      background: "linear-gradient(to bottom,  #ffffff 0%,#EFEFEF 100%)",
+    },
+    "& li:nth-child(odd)": {
+      transform: "perspective(100px) rotateX(-3deg) translateY(3px)",
+      background: "linear-gradient(to top,  #ffffff 0%,#EFEFEF 100%)",
+    }
   },
-  // span.population {
-  //   font-size: 15px;
-  // }
-
-  // .hl {
-  //   background:#ffc600;
-  // }
+  population: {
+    fontSize: "15px",
+  },
+  hl: {
+    background: "#ffc600",
+  }
 }));
 
 const CitySearch = () => {
   const classes = useStyles();
+  const [cities, setCities] = useState([]);
+  const [cityValue, setCityValue] = useState("");
+
+  const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+
+  useEffect(() => {
+    fetch(endpoint)
+      .then(blob => blob.json())
+      .then(data => setCities([...data]))
+  }, [cityValue]);
+
+  function findMatches(wordToMatch, cities) {
+    return cities.filter(place => {
+      const regex = new RegExp(wordToMatch, 'gi');
+
+      return place.city.match(regex) || place.state.match(regex);
+    });
+  }
+
+  function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  }
+
+  const displayMatches = useCallback(() => {
+    if (!cityValue) return;
+    
+    const matchArray = findMatches(cityValue, cities);
+    const html = matchArray.map((place, index)=> {
+      const regex = new RegExp(cityValue, 'gi');
+      const cityName = place.city.replace(regex, cityValue);
+      const stateName = place.state.replace(regex, cityValue);
+      return (
+        <li key={index}>
+          <span>
+            <span className={classes.hl}>{cityName}</span>,
+            <span className={classes.hl}>{stateName}</span>
+          </span>
+          <span className={classes.population}>{`${numberWithCommas(place.population)}`}</span>
+        </li>
+      );
+    });
+
+    return html;
+  }, [cityValue, setCityValue])
+
 
   return (
     <Box className={classes.root}>
       <form className={classes.searchForm}>
-        <input type="text" className={classes.search} placeholder="City or State" />
+        <input
+          type="text"
+          className={classes.search}
+          placeholder="U.S. City or State"
+          value={cityValue}
+          onChange={(e) => setCityValue(e.target.value)}
+        />
         <ul className={classes.suggestions}>
           <li>Filter for a city</li>
           <li>or a state</li>
+          {displayMatches()}
         </ul>
       </form>
     </Box>
